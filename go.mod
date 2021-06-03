b2385d3797064a6843a7d2fa912e3b6187b444a8
mopp <hello@mopp.jp>
2021-06-03 22:07:00 +0900

    construct minimal layout

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mopp/gote
 
 go 1.16
+
+require (
+	github.com/gcla/gowid v1.2.0
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gcla/gowid"
+	"github.com/gcla/gowid/widgets/columns"
+	"github.com/gcla/gowid/widgets/divider"
+	"github.com/gcla/gowid/widgets/edit"
+	"github.com/gcla/gowid/widgets/fill"
+	"github.com/gcla/gowid/widgets/framed"
+	"github.com/gcla/gowid/widgets/pile"
+	"github.com/gcla/gowid/widgets/text"
+	"github.com/gcla/gowid/widgets/vpadding"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	text1 := text.New("hello world")
+
+	titles := pile.New([]gowid.IContainerWidget{
+		&gowid.ContainerWidget{IWidget: text1, D: gowid.RenderWithWeight{W: 1}},
+	})
+
+	editor := edit.New(edit.Options{Caption: "main\n", Text: "abcde"})
+	statusLine := text.New("hello world\nhi")
+
+	main := pile.New([]gowid.IContainerWidget{
+		&gowid.ContainerWidget{IWidget: editor, D: gowid.RenderWithWeight{W: 1}},
+		&gowid.ContainerWidget{IWidget: divider.NewAscii(), D: gowid.RenderFlow{}},
+		&gowid.ContainerWidget{IWidget: statusLine, D: gowid.RenderWithUnits{U: 1}},
+	})
+
+	keywords := pile.New([]gowid.IContainerWidget{
+		&gowid.ContainerWidget{IWidget: text.New("Keywords"), D: gowid.RenderWithWeight{W: 1}},
+		&gowid.ContainerWidget{IWidget: divider.NewAscii(), D: gowid.RenderFlow{}},
+		&gowid.ContainerWidget{IWidget: text.New("Relation"), D: gowid.RenderWithWeight{W: 1}},
+	})
+
+	vline := fill.New('|')
+	content := columns.New([]gowid.IContainerWidget{
+		&gowid.ContainerWidget{IWidget: vpadding.New(titles, gowid.VAlignTop{}, gowid.RenderFlow{}), D: gowid.RenderWithWeight{W: 1}},
+		&gowid.ContainerWidget{IWidget: vline, D: gowid.RenderWithUnits{U: 1}},
+		&gowid.ContainerWidget{IWidget: main, D: gowid.RenderWithWeight{W: 2}},
+		&gowid.ContainerWidget{IWidget: vline, D: gowid.RenderWithUnits{U: 1}},
+		&gowid.ContainerWidget{IWidget: keywords, D: gowid.RenderWithWeight{W: 1}},
+	})
+
+	view := framed.New(content, framed.Options{
+		Frame:       framed.AsciiFrame,
+		TitleWidget: text.New("Gote"),
+	})
+
+	app, _ := gowid.NewApp(gowid.AppArgs{
+		View: view,
+		Log:  log.StandardLogger(),
+	})
+
+	app.SimpleMainLoop()
 }
