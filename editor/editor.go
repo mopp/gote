d35537ab9c85056d1e17c12901216b21fa4a51e6
mopp <hello@mopp.jp>
2021-06-14 22:11:54 +0900

    implement editor.Widget

---
+++ editor/editor.go
@@ -0,0 +1,57 @@
+package editor
+
+import (
+	"github.com/gcla/gowid"
+	"github.com/gcla/gowid/widgets/divider"
+	"github.com/gcla/gowid/widgets/edit"
+	"github.com/gcla/gowid/widgets/pile"
+	"github.com/gcla/gowid/widgets/text"
+	"github.com/gdamore/tcell"
+	log "github.com/sirupsen/logrus"
+)
+
+type Widget struct {
+	*pile.Widget
+}
+
+func New(t string) *Widget {
+	e := edit.New(edit.Options{Text: t})
+	statusLine := text.New("Started.")
+
+	return &Widget{
+		Widget: pile.New([]gowid.IContainerWidget{
+			&gowid.ContainerWidget{IWidget: e, D: gowid.RenderWithWeight{W: 1}},
+			&gowid.ContainerWidget{IWidget: divider.NewAscii(), D: gowid.RenderFlow{}},
+			&gowid.ContainerWidget{IWidget: statusLine, D: gowid.RenderWithUnits{U: 1}},
+		}),
+	}
+}
+
+func (w *Widget) edit() *edit.Widget {
+	return w.SubWidgets()[0].(*gowid.ContainerWidget).SubWidget().(*edit.Widget)
+}
+
+func (w *Widget) statusLine() *text.Widget {
+	return w.SubWidgets()[2].(*gowid.ContainerWidget).SubWidget().(*text.Widget)
+}
+
+func (w *Widget) UserInput(ev interface{}, size gowid.IRenderSize, focus gowid.Selector, app gowid.IApp) bool {
+	evk, ok := ev.(*tcell.EventKey)
+	if !ok {
+		return false
+	}
+
+	if evk.Key() == tcell.KeyCtrlS {
+		w.statusLine().SetText("Saved.", app)
+		// TODO: Implement callback.
+		log.Info("Saved.")
+		log.Info(w.edit().Text())
+
+		return true
+	}
+
+	// TODO: Clear status line using chanel.
+	w.statusLine().SetText("", app)
+
+	return w.edit().UserInput(ev, size, focus, app)
+}
--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"github.com/gcla/gowid"
 	"github.com/gcla/gowid/widgets/columns"
 	"github.com/gcla/gowid/widgets/divider"
-	"github.com/gcla/gowid/widgets/edit"
 	"github.com/gcla/gowid/widgets/fill"
 	"github.com/gcla/gowid/widgets/framed"
 	"github.com/gcla/gowid/widgets/pile"
 	"github.com/gcla/gowid/widgets/text"
 	"github.com/gcla/gowid/widgets/vpadding"
+	"github.com/mopp/gote/editor"
 	"github.com/mopp/gote/titles"
 	log "github.com/sirupsen/logrus"
 )
@@ -27,15 +27,9 @@ func main() {
 		"ccc",
 	})
 
-	editor := edit.New(edit.Options{Text: "abcde"})
-	statusLine := text.New("status line")
+	editor := editor.New("this is a pen.")
 
-	main := pile.New([]gowid.IContainerWidget{
-		&gowid.ContainerWidget{IWidget: editor, D: gowid.RenderWithWeight{W: 1}},
-		&gowid.ContainerWidget{IWidget: divider.NewAscii(), D: gowid.RenderFlow{}},
-		&gowid.ContainerWidget{IWidget: statusLine, D: gowid.RenderWithUnits{U: 1}},
-	})
-
+	// TODO
 	keywords := pile.New([]gowid.IContainerWidget{
 		&gowid.ContainerWidget{IWidget: text.New("Keywords"), D: gowid.RenderWithWeight{W: 1}},
 		&gowid.ContainerWidget{IWidget: divider.NewAscii(), D: gowid.RenderFlow{}},
@@ -46,7 +40,7 @@ func main() {
 	content := columns.New([]gowid.IContainerWidget{
 		&gowid.ContainerWidget{IWidget: vpadding.New(titles, gowid.VAlignTop{}, gowid.RenderFlow{}), D: gowid.RenderWithWeight{W: 1}},
 		&gowid.ContainerWidget{IWidget: vline, D: gowid.RenderWithUnits{U: 1}},
-		&gowid.ContainerWidget{IWidget: main, D: gowid.RenderWithWeight{W: 2}},
+		&gowid.ContainerWidget{IWidget: editor, D: gowid.RenderWithWeight{W: 2}},
 		&gowid.ContainerWidget{IWidget: vline, D: gowid.RenderWithUnits{U: 1}},
 		&gowid.ContainerWidget{IWidget: keywords, D: gowid.RenderWithWeight{W: 1}},
 	})
